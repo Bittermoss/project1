9bb4bde0b43849277d784db063aac48638e7ee10
Girls Who Code <gwc@csad.cs.calvin.edu>
2023-02-03 20:04:52 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <cassert>
 using namespace std;
 
+double constrain(double value, double low, double high){
+    if(value > high){
+        return high; 
+    }else if(value < low){
+        return low; 
+    }else{
+        return value; 
+    }
+}
+
 int main() {
-    cout<<"hi "<<endl;
+
+    // assert(constrain(100, 100, 100)); 
+    // assert(constrain(100, 50, 100)); 
+    // assert(constrain(50, 50, 100)); 
+    // assert(constrain(0, -3, 3));
+    // assert(constrain(-3, -3, 3));
+    // assert(constrain(3, -3, 3));
+
+     cout<<"Assert tests completed"<<endl;
+
     return 0;
 }
